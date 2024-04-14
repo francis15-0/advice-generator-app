48b876cf7fca615c79927808f6c3c840da4c2187
francis15-0 <nnadiekwemakuo.11@gmail.com>
2024-04-14 02:02:58 -0400

    finished the html and css

---
+++ main.js
@@ -0,0 +1,23 @@
+const paragraph = document.querySelector('#text');
+const button = document.querySelector('.circle');
+const advice = document.querySelector('.advice-id');
+button.addEventListener('click', ()=>{
+    getAdvice().then((data)=>{
+        console.log(data.slip.advice);
+        paragraph.textContent = '';
+        advice.textContent = '';
+        paragraph.textContent = `"${ data.slip.advice}"`;
+        advice.textContent = `Advice # ${data.slip.id}`;
+    }).catch(()=>{})
+
+});
+
+
+const getAdvice = async()=>{
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    if (!res.ok) {
+        throw new Error("something happened");
+    }
+    return data;
+}
